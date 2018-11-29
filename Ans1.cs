aff37c5ea64a5af6ebdb5aeb1e63875e9ee79870
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2018-11-29 15:55:43 +0200

    HW - Ans1

---
+++ Ans1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students are in class?");
+            int StudentAmount = Convert.ToInt16(Console.ReadLine());
+            int[] array = new int[StudentAmount];
+
+            for (int i = 0; i < StudentAmount; i++)
+            {
+                Console.WriteLine("what is the grade?");
+                array[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            int sum = 0, avg = 0;
+            for (int i = 0; i < StudentAmount; i++)
+            {
+                sum += array[i];
+            }
+            avg = sum / StudentAmount;
+            Console.WriteLine("sum is: " + sum + " avg is: " +avg);
+
+            Console.ReadLine();
+        }
+    }
+}
